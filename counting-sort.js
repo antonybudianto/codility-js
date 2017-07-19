4995636eeba6a736c0167e2b05f37bbc066c79db
Antony Budianto <antonybudianto@gmail.com>
2017-07-19 20:59:09 +0700

    Create counting-sort.js

---
+++ counting-sort.js
@@ -0,0 +1,15 @@
+// Optimized for space
+
+function countingSort(A, min, max) {
+  let len = max - min;
+  let counters = new Array(len);
+  for(let i=0;i<len;i++) counters[i] = 0;
+  let alen = A.length;
+  for(let i=0;i<alen;i++) counters[A[i]-min]++
+  let arr = [];
+  for(let i=0;i<len;i++) {
+    for(let j=0;j<counters[i];j++) arr.push(min+i)
+  }
+  
+  return arr;
+}

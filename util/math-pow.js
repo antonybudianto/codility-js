1cbbb952a0fcdc6e98fd0d9c7e31155798f55d4e
Antony Budianto <antonybudianto@gmail.com>
2017-07-27 11:06:33 +0700

    Update math-pow.js

--- util/math-pow.js
+++ util/math-pow.js
@@ -1,5 +1,5 @@
-function power(x,y){
-  if(y===0){return 1}
+function power(x, y){
+  if(y === 0) { return 1; }
   let newY = parseInt(y/2);
   return (y % 2 === 0 ? 1 : x) * power(x, newY) * power(x, newY);
 }

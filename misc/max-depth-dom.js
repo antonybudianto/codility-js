24d7e93491eeb97ed5d338f5ee93d668ac6873b8
Antony Budianto <antonybudianto@gmail.com>
2017-08-17 19:35:59 +0700

    Update max-depth-dom.js

--- misc/max-depth-dom.js
+++ misc/max-depth-dom.js
@@ -3,15 +3,14 @@ function solution(N) {
   let count=[0,0,0]
    children.each((i,a) => {
     let achild = $(a).children();
-    
-	  while(achild.length) {
-     achild.each((idx,b) => {
-       let bool = ['ul','ol'].indexOf(b.localName) !== -1;
-       if (bool) count[i]++;
-     });
-    
-     achild = achild.children();
-  	}
+    while(achild.length) {
+       achild.each((idx,b) => {
+         let bool = ['ul','ol'].indexOf(b.localName) !== -1;
+         if (bool) count[i]++;
+       });
+   
+       achild = achild.children();
+    }
   });
   
   return count.reduce((a,b) => a > b ? a : b, 0)
